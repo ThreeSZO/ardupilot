0f7b065e6bac2d4a7829f5e4071aa065cc9b7211
ThreeSZO <fly3601@gmail.com>
2014-09-16 23:32:53 +0800

    add KS103 driver

---
+++ libraries/AP_RangeFinder/AP_RangeFinder_KS103I2C.cpp
@@ -0,0 +1,99 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ *       AP_RangeFinder_KS103I2C.cpp - Arduino Library for KS103 I2C sonar
+ *       Code by 3601. EulerSpace.com
+ *
+ *       datasheet:
+ *
+ *       Sensor should be connected to the I2C port
+ */
+
+ #include "AP_RangeFinder_KS103I2C.h"
+ #include <AP_HAL.h>
+
+ extern const AP_HAL::HAL& hal;
+
+ AP_RangeFinder_KS103I2C::AP_RangeFinder_KS103I2C(RangeFinder &_ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state) :
+    AP_RangeFinder_Backend(_ranger, instance, _state)
+{
+}
+
+bool AP_RangeFinder_KS103I2C::detect(RangeFinder &_ranger, uint8_t instance)
+{
+   if(!start_reading()){
+      return false;
+   }
+
+   //hal.scheduler->delay(50);
+   uint16_t reading_mm;
+   return get_reading(reading_mm);
+}
+
+bool AP_RangeFinder_KS103I2C::start_reading()
+{
+   // get pointer to i2c bus semaphore
+    AP_HAL::Semaphore* i2c_sem = hal.i2c->get_semaphore();
+
+    // exit immediately if we can't take the semaphore
+    if (i2c_sem == NULL || !i2c_sem->take(1)) {
+        return false;
+    }
+
+    uint8_t tosend[1] =
+        { AP_RANGE_FINDER_KS103I2C_COMMAND_TAKE_RANGE };
+
+   hal.i2c->setHighSpeed(false);
+
+   /*if(hal.i2c->write(AP_RANGE_FINDER_KS103I2C_DEFAULT_ADDR,
+                     1, tosend) != 0)*/
+   if(hal.i2c->writeRegisters(AP_RANGE_FINDER_KS103I2C_DEFAULT_ADDR,
+                              0x02, 1, tosend) != 0)
+   {
+      i2c_sem->give();
+      return false;
+   }
+
+   i2c_sem->give();
+
+   return true;
+}
+
+bool AP_RangeFinder_KS103I2C::get_reading(uint16_t &reading_mm)
+{
+   uint8_t buff[2];
+
+   AP_HAL::Semaphore* i2c_sem = hal.i2c->get_semaphore();
+
+   if (hal.i2c->read(AP_RANGE_FINDER_KS103I2C_DEFAULT_ADDR, 2, buff) != 0) {
+      i2c_sem->give();
+      return false;
+   }
+
+   i2c_sem->give();
+
+   reading_mm = ((uint16_t)buff[0]) << 8 | buff[1];
+
+   start_reading();
+
+   return true;
+}
+
+void AP_RangeFinder_KS103I2C::update(void)
+{
+   state.healthy = get_reading(state.distance_cm);
+}
---
+++ libraries/AP_RangeFinder/AP_RangeFinder_KS103I2C.h
@@ -0,0 +1,31 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+#ifndef __AP_RANGEFINDER_KS103I2C_H__
+#define __AP_RANGEFINDER_KS103I2C_H__
+
+#include "RangeFinder.h"
+#include "RangeFinder_Backend.h"
+
+#define AP_RANGE_FINDER_KS103I2C_DEFAULT_ADDR (0xd6)
+
+#define AP_RANGE_FINDER_KS103I2C_COMMAND_TAKE_RANGE 0xba	//0-11m, return mm, max time 68ms
+
+class AP_RangeFinder_KS103I2C : public AP_RangeFinder_Backend
+{
+public:
+    // constructor
+    AP_RangeFinder_KS103I2C(RangeFinder &ranger, uint8_t instance, RangeFinder::RangeFinder_State &_state);
+
+    // static detection function
+    static bool detect(RangeFinder &ranger, uint8_t instance);
+
+    // update state
+    void update(void);
+
+private:
+    // start a reading
+    static bool start_reading(void);
+    static bool get_reading(uint16_t &reading_mm);
+};
+
+#endif	//__AP_RANGEFINDER_KS103I2C_H__
